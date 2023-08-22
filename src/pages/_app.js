dd620ddf51aec907af061e69f44282c212262039
bioDanCho <daniel.cho@thebioagency.com>
2023-08-22 09:19:52 -0400

    add widget

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css';
+import BioStorybookWidget from 'bio-storybook-widget';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <BioStorybookWidget projectSource={'monzo'} />
+        </>
+    );
 }
